f0f432cbb5b72057de4eafedd79135ff374b52cb
Am10aN16 <2020071013@mmmut.ac.in>
2022-09-23 00:34:14 +0530

    updqate

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -119,6 +119,7 @@ import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings1 = ['Profile', 'Signup', 'Dashboard', 'Login'];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
